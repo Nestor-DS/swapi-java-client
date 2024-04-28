04c7602e4c930d7474d8382a5c232158aa7936a7
Nestor-DS <nestorduhamel18@outlook.es>
2024-04-28 02:18:33 -0600

    Movie record creation

---
+++ src/com/nds/starwarsapi/Movie.java
@@ -0,0 +1,9 @@
+package com.nds.starwarsapi;
+
+public record Movie(String title,
+                    int episode_id,
+                    String opening_crawl,
+                    String director,
+                    String producer,
+                    String release_date) {
+}
